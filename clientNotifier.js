d1ec32771db06b562f733efd7e5538f996689a01
Giancarlo Orru <techlab4kids.apps@gmail.com>
2022-10-28 19:36:19 +0200

    Implements api rest

--- clientNotifier.js
+++ clientNotifier.js
@@ -3,13 +3,13 @@ function notifyClient() {
     let server = "localhost:5500"
     let client = {hostname: "pippo", ip: "192.168.10.1"}
 
-    get(`http://${server}/`).then((result) => {
+    get(`http://${server}/api/`).then((result) => {
         console.log(`result: ${result.data.message}`);
     }).catch(function (error) {
         console.log(`Connection failed: ${error}`);
     })
 
-    post(`http://${server}/clients`, client).then((result) => {
+    post(`http://${server}/api/clients`, client).then((result) => {
         console.log(`Client registered:  ${result.data.message}`);
     }).catch(function (error) {
         console.log(`Client registration to Client Manager failed ${error}`);
